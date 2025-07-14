e318651d711922401e7dbc71bf9c1391daa5bfe8
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-14 11:21:32 -0400

    Class cola, Cola.hpp, Cola.tpp, constructor

---
+++ Cola.hpp
@@ -0,0 +1,23 @@
+#ifndef COLA_HPP_INCLUDED
+#define COLA_HPP_INCLUDED
+
+template <typename T>
+class Cola
+{
+public:
+    Cola();
+
+private:
+    int tam;
+    struct Elemento
+    {
+        T valor;
+        Elemento *siguiente;
+        Elemento(T val, Elemento *sig) : valor(val), siguiente(sig) {} // Constructor for Elemento
+    };
+    Elemento *fondo;
+};
+
+#include "Cola.tpp"
+
+#endif // COLA_HPP_INCLUDED
---
+++ Cola.tpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Cola.hpp"
+
+//**********************************************
+template <typename T>
+Cola<T>::Cola() : tam(0), fondo(nullptr)
+{
+}
